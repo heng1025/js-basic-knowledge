441bd334b79ab2c9fd9ea9b15e1f5c8c5c9430b9
iron <iron@Mac-mini.local>
2020-08-28 00:54:46 +0800

    change webpack config

--- packages/webpack-101/webpack.assets.js
+++ packages/webpack-101/webpack.assets.js
@@ -1,14 +1,51 @@
-const path = require("path");
+const path = require('path')
+// 根据模版生成html文件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 分离css到单独的文件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// 优化css
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+// 优化js
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.[hash:8].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
-};
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
+  devServer: {
+    port: 8000,
+    progress: true,
+    contentBase: './dist',
+    compress: true,
+  },
+}
--- packages/webpack-101/webpack.config.js
+++ packages/webpack-101/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/base.js",
   output: {
-    filename: "[name]-[hash:8].js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
   mode: "development",
