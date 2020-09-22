1fb7afbcdd884af7ed1ffd7fa718ee2742352e71
iron <472263356@qq.com>
2020-09-22 16:01:54 +0800

    :sparkles: add requestIdleCallback

--- packages/ironeact/index.js
+++ packages/ironeact/index.js
@@ -50,8 +50,22 @@ const IReact = {
   render,
 }
 
-const root = document.getElementById('root')
+// Concurrent Mode
+let nextUnitOfWork = null
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
 
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(nextUnitOfWork) {}
+
+const root = document.getElementById('root')
 const node = IReact.createElement('h1', { title: 'foo' }, 'Hello World')
 console.log('node', node)
 IReact.render(node, root)
